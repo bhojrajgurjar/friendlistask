bcf7fcd5123b0b27f2171550d3dc1418a046a3f9
DELL LPPY <bgurjar2000@gmail.com>
2021-01-15 17:44:30 +0530

    forthcommit

--- app.js
+++ app.js
@@ -1,65 +1,71 @@
-var click = document.getElementById('button').addEventListener('click',clickfunction)
+var click = document.getElementById('button').addEventListener('click', clickfunction)
 var search = document.getElementById('searchbar')
-function clickfunction(){
-  
-   var ritik = document.createElement('li')
-   ritik.id = 'new'
-   ritik.className = 'list-element'
-   ritik.innerHTML = '<p>'+search.value+'</p><i class="fa fa-minus-square" id="icon" aria-hidden="true"></i>'
-   var ul = document.getElementById('list');
-   var li = document.getElementById('ritu');
-   ul.insertBefore(ritik,li);
-   console.log(ritik);
-   search.value=''
-   var removeAdded = document.getElementById('icon').addEventListener('click',newfriend)
+count = 0;
+function clickfunction() {
+   if (searchbar.value == null || searchbar.value == "") {
+      alert("Enter Friend Name")
+   }
+   else {
+      console.log(++count)
+      var ritik = document.createElement('li')
+      ritik.id = 'new'
+      ritik.className = 'list-element''++count'
+      ritik.innerHTML = '<p>' + search.value + '</p><i class="fa fa-minus-square" id="icon" aria-hidden="true"></i>'
+      var ul = document.getElementById('list');
+      var li = document.getElementById('ritu');
+      ul.insertBefore(ritik, li);
+      console.log(ritik);
+      search.value = ''
+      var removeAdded = document.getElementById('icon').addEventListener('click', newfriend)
 
 
-function newfriend(){
-   var newadded = document.getElementById('new')
-   newadded.remove();
+      function newfriend() {
+         var newadded = document.getElementById('new')
+         newadded.remove();
+      }
+   }
 }
-}   
 
 
 
-var toremove = document.getElementById('tomar').addEventListener('click',removefunction)
+var toremove = document.getElementById('tomar').addEventListener('click', removefunction)
 
 
-function removefunction(){
+function removefunction() {
    var rituraj = document.getElementById('ritu')
    rituraj.remove();
 }
 
-var remove = document.getElementById('patel').addEventListener('click',removebhanu)
+var remove = document.getElementById('patel').addEventListener('click', removebhanu)
 
-function removebhanu(){
+function removebhanu() {
    var bhwna = document.getElementById('bhanu')
    bhwna.remove();
 }
-var toRemove = document.getElementById('gupta').addEventListener('click',removeFunction)
+var toRemove = document.getElementById('gupta').addEventListener('click', removeFunction)
 
 
-function removeFunction(){
+function removeFunction() {
    var avnti = document.getElementById('avnti')
    avnti.remove();
 }
-var Remove = document.getElementById('yadav').addEventListener('click',removeyadav)
+var Remove = document.getElementById('yadav').addEventListener('click', removeyadav)
 
-function removeyadav(){
+function removeyadav() {
    var rnjit = document.getElementById('rnjit')
-  rnjit.remove();
+   rnjit.remove();
 }
 
 //var removeAdded = document.getElementById('icon').addEventListener('click',newfriend)
 
 //function newfriend(){
- //  var newadded = document.getElementById('new');
- //  newadded.remove();
+//  var newadded = document.getElementById('new');
+//  newadded.remove();
 //}
-var removeAdded = document.getElementById('icon').addEventListener('click',newfriend)
+var removeAdded = document.getElementById('icon').addEventListener('click', newfriend)
 
 
-function newfriend(){
+function newfriend() {
    var newadded = document.getElementById('new')
    newadded.remove();
 }
